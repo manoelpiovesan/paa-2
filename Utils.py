acb98acfd9437a40250664c43b9ad86f84cb2958
Manoel <manoelpiovesan@gmail.com>
2024-04-18 13:28:21 -0300

    adding more complexity tests

--- Utils.py
+++ Utils.py
@@ -1,6 +1,9 @@
 #
 # Classe com métodos úteis para o projeto.
 # 
+import random
+
+
 class Utils:
     def __init__(self):
         pass
@@ -28,3 +31,28 @@ class Utils:
         for i in range(length):
             arr.append(length-i)
         return arr
+
+    #
+    # Retorna o caso médio de um array de tamanho n sem repetições de elementos.
+    #
+    @staticmethod
+    def averageCaseArrayByLength(length):
+        arr = []
+        for i in range(length):
+            arr.append(i+1)
+        # embaralhando o array
+        for i in range(length):
+            j = random.randint(0, length-1)
+            arr[i], arr[j] = arr[j], arr[i]
+        return arr
+    
+    #
+    # Retorna o melhor caso de um array de tamanho n sem repetições de elementos.
+    #
+    @staticmethod
+    def bestCaseArrayByLength(length):
+        arr = []
+        for i in range(length):
+            arr.append(i+1)
+        return arr
+        
